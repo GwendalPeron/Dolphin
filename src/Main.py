d5f7c4ba7dc8c1c99e2268c990270edf69b8c33b
Kheryo <kheryo@outlook.com>
2017-11-27 08:42:02 +0100

    main - data prep functions 1/2

--- src/Main.py
+++ src/Main.py
@@ -1,11 +1,24 @@
 import APIWhisperer as AW
 import Optimizer as OP
+import json
 import sys
 
 def main(argv):
     api = AW.APIWhisperer()
     opt = OP.Optimizer()
     print(api.getRatios(253))
+
+def extract_dates(quote):
+    data = json.loads(quote) # potentially useless
+    dates = [item["date" for item in data]
+    return dates
+
+def extract_returns(quote):
+    data = json.loads(quote) # potentially useless
+    returns = []
+    for item in data:
+        returns.append(item["return"])
+    return returns
 
 if __name__ == "__main__":
     main(sys.argv[1:])
