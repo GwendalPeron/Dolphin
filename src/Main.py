a417ddf1381212068c17e3aac460c5ee9b8bdc4a
Kheryo <kheryo@outlook.com>
2017-11-27 08:51:45 +0100

    main - data prep functions 2/2

--- src/Main.py
+++ src/Main.py
@@ -1,5 +1,7 @@
 import APIWhisperer as AW
 import Optimizer as OP
+import numpy as np
+import pandas as pd
 import json
 import sys
 
@@ -19,6 +21,23 @@ def extract_returns(quote):
     for item in data:
         returns.append(item["return"])
     return returns
+
+def prep_data(assetids):
+    assets = assetids
+    dates = [] # TODO get dates from first quote
+    data = []
+    for id in assetids:
+        quote = "{}" # TODO get quote from APIWhisperer
+        rets = extract_returns(quote)
+        data.append(rets)
+    data = np.array(data)
+
+    returns = pd.DataFrame(data, columns=assets, index=dates)
+    avg_rets = returns.mean()
+    cov_mat = returns.cov()
+    
+    return returns, cov_mat, avg_rets
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
